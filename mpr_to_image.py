c75716734c9aca13ddf83a4fd0ba7556caeb1432
Matthew Durkin <48136309+MattJDurkin@users.noreply.github.com>
2021-10-24 18:30:05 -0400

    sorting kraggle photos into category subfolders

--- mpr_to_image.py
+++ mpr_to_image.py
@@ -1,12 +1,15 @@
 import cv2
 
 
-vid = cv2.VideoCapture('Target_Tracking_Sample_Videos/Sample_Ships_One.mp4')
+vid = cv2.VideoCapture('Target_Tracking_Sample_Videos/SampleShip3.mp4')
 
 success, image = vid.read()
 count = 1
+arr = []
+
 while success:
     cv2.imwrite("video_data/frame_%d.jpg" % count, image)
+    arr.append("video_data/frame_%d.jpg" % (count))
     success, image = vid.read()
-    print ('Saved image ', count)
+    print (arr[count - 1])
     count += 1
